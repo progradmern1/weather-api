1feb87f83145d963249e6800aaf3d380baf25593
Prasanna-CSE <prasannal2003@gmail.com>
2023-09-25 11:04:48 +0530

    Weather app created successfully

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import WeatherComponent from './Components/WeatherComponent/WeatherComponent'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <WeatherComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App  
